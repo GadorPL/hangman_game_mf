e6526373fd856865eb65101bbf05f92211b5a1ff
Michał Hewelt <mhewelt9@gmail.com>
2022-11-18 18:34:50 +0100

    replace blanks with guesses

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import os
 import random
+
+
+def clear_console():
+    os.system('cls')
+
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-guess = input("Guess a letter: ").lower()
 
+display = []
 for letter in chosen_word:
+    display.append("_")
+print(display)
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+guess = input("Guess a letter: ").lower()
+word_len = len(chosen_word)
+
+index = -1
+for letter in chosen_word:
+    index += 1
     if guess == letter:
+        display[index] = letter
         print("Right")
     else:
         print("Wrong")
+
+clear_console()
+print(display)
